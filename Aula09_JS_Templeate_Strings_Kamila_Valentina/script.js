ddab871059f47fcee010b5ad331d63fb39f26cd4
KamilaMarcon <kamila.marcon@escola.pr.gov.br>
2024-10-28 08:23:46 -0300

    Add files via upload

---
+++ Aula09_JS_Templeate_Strings_Kamila_Valentina/script.js
@@ -0,0 +1,10 @@
+let nome = "Kamila";
+let sobreNome = "Marcon";
+let idade = 16;
+
+let mensagem = "Meu nome é "+nome+" "+sobreNome+" e minha idade é "+idade+" anos.\n";
+console.log(mensagem);
+
+let mensagem2=`Meu nome é ${nome} ${sobreNome} e minha idade é ${idade} anos.`;
+console.log(mensagem2);
+
